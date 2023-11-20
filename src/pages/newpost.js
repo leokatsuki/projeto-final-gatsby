336043fc4250ba73578fa2a47033d0599fcba3ac
leokatsuki <leok-12@hotmail.com>
2023-11-20 16:08:54 -0300

    newpost fix

--- src/pages/newpost.js
+++ src/pages/newpost.js
@@ -10,34 +10,36 @@ function NewPostPage() {
           Preencha os campos abaixo para adicionar um novo post ao blog.
         </p>
 
-        <div class=" grid-3">
-            <h4 class="mb-1">Data</h4>
-            <input type="date" name="data" id="" />
-        </div>
+        <form>
+          <div class=" grid-3">
+              <h4 class="mb-1">Data</h4>
+              <input type="date" name="data" id="" required/>
+          </div>
 
-        <div class="grid-6">
-            <h4 class="mb-1">Título</h4>
-            <input type="text" name="title" id="" />
-        </div>
+          <div class="grid-6">
+              <h4 class="mb-1">Título</h4>
+              <input type="text" name="title" id="" required/>
+          </div>
 
-        <div className='grid-3'>
-            <h4 class="mb-1">Adicionar imagem</h4>
-            <input type="file" name="title" id="" />
-        </div>
+          <div className='grid-3'>
+              <h4 class="mb-1">Adicionar imagem</h4>
+              <input type="file" name="title" id="" required/>
+          </div>
 
-        <div class="grid-12">
-            <h4 class="mb-1">Resumo do post</h4>
-            <input type="text" name="title" id="" />
-        </div>
+          <div class="grid-12">
+              <h4 class="mb-1">Resumo do post</h4>
+              <input type="text" name="title" id="" required/>
+          </div>
 
-        <div class="grid-12">
-            <h4 class=" mb-1">Descrição</h4>
-            <textarea name="description" id="" rows="8"></textarea>
-        </div>
+          <div class="grid-12">
+              <h4 class=" mb-1">Descrição</h4>
+              <textarea name="description" id="" rows="8" required></textarea>
+          </div>
 
-        <div className='grid-12 flex-end-row'>
-            <button class="btn">Adicionar</button>
-        </div>
+          <div className='grid-12 flex-end-row'>
+              <button class="btn" type='submit'>Adicionar</button>
+          </div>
+        </form>
       </section>
     </Layout>
   );
